a84266cd7846657d3814be0e66482124a78acd7e
Arunesh <hello@arunesh.me>
2020-01-14 14:39:52 +0100

    Log if worker died

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@
 import cluster from 'cluster'
 import os from 'os'
 
-if (cluster.isMaster) {
-  console.log(0)
+if (!process.env.DISABLE_CLUSTER && cluster.isMaster) {
   const numCPUs = os.cpus().length;
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork()
   }
+
+  cluster.on('exit', worker => {
+    console.log(`Worker ${worker.process.pid} died`)
+  })
 } else {
-  console.log(1)
   require('./initialize')
 }
