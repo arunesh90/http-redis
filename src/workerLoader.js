06bbb88383e8ea19b7bcc628ef3aaae1b1b98ad5
Arunesh <hello@arunesh.me>
2020-01-14 11:41:47 +0100

    Make workerLoader work with transpiled worker

--- src/workerLoader.js
+++ src/workerLoader.js
@@ -1,4 +1,12 @@
 const path = require('path')
+const fs   = require('fs')
 
-require('ts-node').register()
-require(path.resolve(__dirname, './redisWorker.ts'))
+const TSworkerFile = path.resolve(__dirname, './redisWorker.ts')
+const JSworkerFile = path.resolve(__dirname, './redisWorker.js')
+
+if (fs.existsSync(TSworkerFile)) {
+  require('ts-node').register()
+  require(TSworkerFile)
+} else {
+  require(JSworkerFile)
+}
